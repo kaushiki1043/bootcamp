d630bda9097ca4a151a9ed8806839b51a85508cd
kaushiki1043 <97346719+kaushiki1043@users.noreply.github.com>
2022-11-06 20:38:17 +0530

    Create bootcamp_assignment3.py

---
+++ bootcamp_assignment3.py
@@ -0,0 +1,14 @@
+# Import a gaming package like Pygame Package Builder
+# A Check for gaps starting at the top of the screen
+# If there is a gap, fill it with elements
+# Check the rows and columns.
+# The arrangement of any 3 similar elements in a row or column crashes those three elements
+# Make the upper elements fall down the screen on crash of the elements below them
+# Place bomb is generated if arrangement of 5 takes place
+# Bomb exchanged with any element crashes 16 elements arround it
+# Place row/column bomb is generated if arrangement of 4 takes place
+# row/column bomb exchanged with similar elements of 4 crashes the respective row/column elements arround it
+# if two bombs are exchanged, all elements on the screen crash
+# Place a timer and target according to the difficulty level selected by the user
+# If user is unable to complete the target in time, crash the screen and print("Failed!")
+# If user is able to complete the target in time, crash the screen and print("Success!") and move to the next level
